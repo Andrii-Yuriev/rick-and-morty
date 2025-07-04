5c02b356117d0256213ee16450fd95a524b5ce7b
“Andrii-Yuriev” <andrii.yuriev.dev@gmail.com>
2025-07-04 10:59:10 +0300

    Added character list endpoint with custom search name field

--- characters/views.py
+++ characters/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 from characters.models import Character
 from characters.serializers import CharacterSerializer
+from rest_framework import generics
+from django.db.models.query import QuerySet
 
 
 @api_view(["GET"])
@@ -17,4 +19,14 @@ def get_random_character(request: Request) -> Response:
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class 
+class CharacterListView(generics.ListAPIView):
+    serializer_class = CharacterSerializer
+
+    def get_queryset(self) -> QuerySet:
+        queryset = Character.objects.all()
+
+        name = self.request.query_params.get("name")
+        if name is not None:
+            queryset = queryset.filter(name__icontains=name)
+
+        return queryset
