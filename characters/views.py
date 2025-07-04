1c776536da3d86d6f2dd05ddb1d11936868edf49
“Andrii-Yuriev” <andrii.yuriev.dev@gmail.com>
2025-07-04 10:50:51 +0300

    feat: Add drf & random chracter view

--- characters/views.py
+++ characters/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import random
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
+from characters.models import Character
+from characters.serializers import CharacterSerializer
 
-# Create your views here.
+
+@api_view(["GET"])
+def get_random_character(request: Request) -> Response:
+    pks = Character.objects.values_list("pk", flat=True)
+    random_pk = random.choice(pks)
+    random_character = Character.objects.get(pk=random_pk)
+    serializer = CharacterSerializer(random_character)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class 
